d9249696614de5a38d5d9618c5f009282252406d
Leeseungmin <59906532+sammundong@users.noreply.github.com>
2022-05-04 10:49:05 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+  name = 'testoutput011031',
+  version = '1.0.0',
+  description = 'oss- development class test ouput',
+  author = 'seungmin',
+  author_email = 'lsls011031@gmail.com',
+  url = 'https://github.com/sammundong/test/',
+  install_requires=['pytest'],
+  long_description = 'oss development...',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+    "programming Language :: python :: 3",
+    "Opereating system :: OS",
+  ],
+)
